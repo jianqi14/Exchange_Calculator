98d9576b829d2286bb55498c8a540c6679c2b8ef
Jian Qi <jianqi1997@gmail.com>
2020-05-03 01:39:42 -0700

    Intial Swift setup

---
+++ exchangeRateCalculator/exchangeRateCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  exchangeRateCalculator
+//
+//  Created by Jian Qi on 5/3/20.
+//  Copyright © 2020 Jian Qi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
