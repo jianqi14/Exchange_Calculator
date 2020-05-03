11452610c8b7d238149d597b2f0f3d0ee70193dd
Jian Qi <jianqi1997@gmail.com>
2020-05-03 01:43:59 -0700

    Created the Select View for first item. Drop down functionality

--- exchangeRateCalculator/exchangeRateCalculator/ViewController.swift
+++ exchangeRateCalculator/exchangeRateCalculator/ViewController.swift
@@ -8,13 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        return countries.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return countries[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectPriority = countries[row]
+        fromCountry.text = selectPriority
+    }
+    
+    var selectPriority: String?
+    var countries = ["United States", "China", "Canada", "Australia", "Korea", "Singapore"]
 
+    func createPickerView(){
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        
+        fromCountry.inputView = pickerView
+    }
+    
+    func dismissPickerView(){
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.dismissKeyboard))
+        
+        toolBar.setItems([doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        
+        fromCountry.inputAccessoryView = toolBar
+    }
+    
+    @objc func dismissKeyboard(){
+        view.endEditing(true)
+    }
+    
+    @IBOutlet weak var fromCountry: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createPickerView()
+        dismissPickerView()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
-
 }
-
