d0d80c48f32c352ec04dbec7d4036e3a08caa3e1
Jian Qi <jianqi1997@gmail.com>
2020-05-31 18:40:56 -0700

    Fixed Both  Individual Picker View to Unique Country

--- exchangeRateCalculator/exchangeRateCalculator/ViewController.swift
+++ exchangeRateCalculator/exchangeRateCalculator/ViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  exchangeRateCalculator
@@ -10,59 +11,80 @@ import UIKit
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int{
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
-        return countries.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return countries[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        selectPriority = countries[row]
-        fromCountry.text = selectPriority
-    }
-    
-    var selectPriority: String?
-    var countries = ["United States", "China", "Canada", "Australia", "Korea", "Singapore"]
-
-    func createPickerView(){
-        let pickerView = UIPickerView()
-        pickerView.delegate = self
-        
-        fromCountry.inputView = pickerView
-    }
-    
-    func dismissPickerView(){
-        let toolBar = UIToolbar()
-        toolBar.sizeToFit()
-        
-        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.dismissKeyboard))
-        
-        toolBar.setItems([doneButton], animated: false)
-        toolBar.isUserInteractionEnabled = true
-        
-        fromCountry.inputAccessoryView = toolBar
-    }
-    
-    @objc func dismissKeyboard(){
-        view.endEditing(true)
-    }
-    
     @IBOutlet weak var fromCountry: UITextField!
+    @IBOutlet weak var toCountry: UITextField!
+    
+    
+    let fromPickView = UIPickerView()
+    let toPickView = UIPickerView()
+    
+    let countryList1 = ["United States", "China", "Canada", "Australia", "Korea", "Singapore"]
+    let countryList2 = ["United States", "China", "Canada", "Australia", "Korea", "Singapore"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        createPickerView()
-        dismissPickerView()
+        
+        fromPickView.dataSource = self
+        fromPickView.delegate = self
+        
+        toPickView.dataSource = self
+        toPickView.delegate = self
+        
+        fromPickView.tag = 1
+        toPickView.tag = 2;
+        
+        fromCountry.inputView = fromPickView
+        toCountry.inputView = toPickView
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        
+        if pickerView == fromPickView {
+            return countryList1.count
+            
+        }else if pickerView == toPickView {
+            return countryList2.count
+            
+        }
+        
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        if pickerView == fromPickView {
+            return countryList1[row]
+            
+        } else if pickerView == toPickView {
+            return countryList2[row]
+            
+        }
+        return ""
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        if pickerView == fromPickView {
+            fromCountry.text = countryList1[row]
+            self.view.endEditing(false)
+            
+        }else if pickerView == toPickView {
+            toCountry.text = countryList2[row]
+            self.view.endEditing(false)
+        }
+    }
 
 }
+
+
+
+
+
